8fc91d72b018ceb58875d911712c0689ac11d069
renatoduttra <renatoduttra@gmail.com>
2022-06-10 23:57:06 -0300

    Comentarios
    atualizando comentários

--- wikipedia basico.py
+++ wikipedia basico.py
@@ -1,12 +1,12 @@
-import wikipedia
+import wikipedia #Biblioteca
 
 """ busca = wikipedia.search('Brasil')
 
 for lista in busca:
     print(wikipedia.summary(lista)) """
 
-busca = 'brasil'
-wikipedia.set_lang('pt')
-pesquisa = wikipedia.summary(busca, 3)
+busca = 'brasil'    #Conteudo da pesquisa
+wikipedia.set_lang('pt') # linguagem
+pesquisa = wikipedia.summary(busca, 3) # conteudo e Quantidade de linhas
 
-print(pesquisa)
+print(pesquisa)# mostra a pesquisa
